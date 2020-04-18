919e4218e5cb0b1b18bfe50a0e6f9e29792f9aef
zhangfan <sofa_fan@163.com>
2020-04-18 11:23:16 +0800

    11

--- js/app.js
+++
@@ -1,6 +0,0 @@
-define(["require", "exports", "./src/MainPage"], function (require, exports, MainPage_1) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    var mainPage = new MainPage_1.default();
-    console.log('=============ccccccccc');
-});
