1f1cd7a682d3f799684a039151a7461296037682
zhangfan <sofa_fan@163.com>
2020-04-23 16:56:31 +0800

    添加webpack插件

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,22 @@
-let path = require('path');
-let webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 module.exports = {
     entry: './js/app.js',
     output: {
         path: path.join(__dirname, 'dist'), //输出目录的配置，模板、样式、脚本、图片等资源的路径配置都相对于它
         filename: "bundle.js"
     },
-    module: {
-        rules: [
-
-        ]
-    }
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Webpack Output Management',
+            filename: 'index.html',
+            // template: 'src/index.html'
+        }),
+        new CleanWebpackPlugin({
+            cleanAfterEveryBuildPatterns: ["dist"],
+        })
+    ]
 };
